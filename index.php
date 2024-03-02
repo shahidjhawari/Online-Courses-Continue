26a6144461ae926bc93fa132eb4f3ef8241128fc
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-02 10:12:21 +0500

    add code placed

--- index.php
+++ index.php
@@ -1,136 +1,193 @@
-<?php require("top.php"); ?>  
+<?php require("top.php"); ?>
 <!--Banner Section Start-->
-  <div id="carouselExampleInterval" class="carousel slide inner-css" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active" data-bs-interval="5000">
-        <img src="img/banner1.webp" class="d-block w-100" alt="..." width="100%" height="100%">
-      </div>
-      <div class="carousel-item" data-bs-interval="2000">
-        <a href="openai.php"><img src="img/ai-banner2.webp" class="d-block w-100" alt="..." width="100%" height="100%"></a>
-      </div>
-      <div class="carousel-item">
-        <img src="img/banner1.webp" class="d-block w-100" alt="..." width="100%" height="100%">
-      </div>
+<div id="carouselExampleInterval" class="carousel slide inner-css" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="5000">
+      <img src="img/banner1.webp" class="d-block w-100" alt="..." width="100%" height="100%">
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="coming_soon.htmlcarouselExampleInterval"
-      data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="coming_soon.htmlcarouselExampleInterval"
-      data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+    <div class="carousel-item" data-bs-interval="2000">
+      <a href="openai.php"><img src="img/ai-banner2.webp" class="d-block w-100" alt="..." width="100%" height="100%"></a>
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner1.webp" class="d-block w-100" alt="..." width="100%" height="100%">
+    </div>
   </div>
-  <!--Banner Section End-->
+  <button class="carousel-control-prev" type="button" data-bs-target="coming_soon.htmlcarouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="coming_soon.htmlcarouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!--Banner Section End-->
 
-  <!--area tag for ai banner Section start-->
-  <!-- <map name="aibanner">
+<!--area tag for ai banner Section start-->
+<!-- <map name="aibanner">
   <area shape="rect" coords="2077,2419,2953,1953" href="openai.php" alt="">
 </map> -->
-  <!--area tag for ai banner Section start-->
+<!--area tag for ai banner Section start-->
 
-  <!-- Courses Heading Start -->
-  <!-- <div class="form">
+<!-- Courses Heading Start -->
+<!-- <div class="form">
     <div class="searchbar">
       <input class="search_input" type="text" name="" placeholder="Search here...">
         <a href="coming_soon.html" class="search_icon"><i class="fa fa-search" aria-hidden="true"></i></a>
     </div>
   </div> -->
-  <!-- Courses Heading End -->
+<!-- Courses Heading End -->
 
-  <!-- Why We are start -->
-  <div class="container-fluid py-1">
-    <div class="container py-1">
-      <div class="row align-items-center">
-        <div class="col-lg-3">
-          <img class="img-fluid rounded mb-4 mb-lg-0" src="img/why.webp" alt="" width="100%" height="100%">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6521349394702804" crossorigin="anonymous"></script>
+<!-- Ad for header -->
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6521349394702804" data-ad-slot="3555489595" data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<!-- Why We are start -->
+<div class="container-fluid py-1">
+  <div class="container py-1">
+    <div class="row align-items-center">
+      <div class="col-lg-3">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="img/why.webp" alt="" width="100%" height="100%">
+      </div>
+      <div class="col-lg-9">
+        <div class="text-left mb-4">
+          <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 1px;">WHY WE ARE</h5>
+          <h1>Jhawarian`s No. #1 Academy</h1>
         </div>
-        <div class="col-lg-9">
-          <div class="text-left mb-4">
-            <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 1px;">WHY WE ARE</h5>
-            <h1>Jhawarian`s No. #1 Academy</h1>
-          </div>
-          <p>Jhawarian's number one academy teaching you website designing and website development as well as android
-            app development, this academy is very famous for its quality and best quality of education, you can check
-            our services, Thanks!</p>
-        </div>
+        <p>Jhawarian's number one academy teaching you website designing and website development as well as android
+          app development, this academy is very famous for its quality and best quality of education, you can check
+          our services, Thanks!</p>
       </div>
     </div>
   </div>
-  <!-- Why We are end -->
+</div>
+<!-- Why We are end -->
 
-  <!-- all course add heading start -->
-  <div class="domain">
-    <div class="titlepage text_align_center">
-      <h2>ALL COURSES <span class="blue_light">HERE</span></h2>
+<!-- all course add heading start -->
+<div class="domain">
+  <div class="titlepage text_align_center">
+    <h2>ALL COURSES <span class="blue_light">HERE</span></h2>
+  </div>
+</div>
+<!-- all course add heading start -->
+
+<!--HTML Section Start-->
+<section class="about_section layout_padding">
+  <div class="container  ">
+    <div class="heading_container heading_center">
+      <h2>HTML5</h2>
+      <p>Let's understand the fundamental and basic concept of HTML</p>
+      <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
+      <a href="html_crc.php">LEARN HTML</a>
     </div>
   </div>
-  <!-- all course add heading start -->
+</section>
+<!--HTML Section End-->
 
-  <!--HTML Section Start-->
-  <section class="about_section layout_padding">
-    <div class="container  ">
-      <div class="heading_container heading_center">
-        <h2>HTML5</h2>
-        <p>Let's understand the fundamental and basic concept of HTML</p>
-        <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
-        <a href="html_crc.php">LEARN HTML</a>
-      </div>
-    </div>
-  </section>
-  <!--HTML Section End-->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6521349394702804"
+     crossorigin="anonymous"></script>
+<!-- Ad for header -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6521349394702804"
+     data-ad-slot="3555489595"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
-  <!--CSS Section Start-->
-  <section class="about_section layout_padding about-css">
-    <div class="container  ">
-      <div class="heading_container heading_center">
-        <h2>CSS3</h2>
-        <p>Let's understand the fundamental and basic concept of HTML</p>
-        <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
-        <a href="coming_soon.php">LEARN CSS</a>
-      </div>
+<!--CSS Section Start-->
+<section class="about_section layout_padding about-css">
+  <div class="container  ">
+    <div class="heading_container heading_center">
+      <h2>CSS3</h2>
+      <p>Let's understand the fundamental and basic concept of HTML</p>
+      <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
+      <a href="coming_soon.php">LEARN CSS</a>
     </div>
-  </section>
-  <!--CSS Section End-->
+  </div>
+</section>
+<!--CSS Section End-->
 
-  <!--JS Section Start-->
-  <section class="about_section layout_padding about-javascript">
-    <div class="container  ">
-      <div class="heading_container heading_center">
-        <h2>JAVASCRIPT</h2>
-        <p>Let's understand the fundamental and basic concept of HTML</p>
-        <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
-        <a href="coming_soon.php">LEARN JAVASCRIPT</a>
-      </div>
-    </div>
-  </section>
-  <!--JS Section End-->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6521349394702804"
+     crossorigin="anonymous"></script>
+<!-- Ad for header -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6521349394702804"
+     data-ad-slot="3555489595"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
-  <!--PHP Section Start-->
-  <section class="about_section layout_padding about-php">
-    <div class="container  ">
-      <div class="heading_container heading_center">
-        <h2>PHP</h2>
-        <p>Let's understand the fundamental and basic concept of HTML</p>
-        <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
-        <a href="coming_soon.php">LEARN PHP</a>
-      </div>
+<!--JS Section Start-->
+<section class="about_section layout_padding about-javascript">
+  <div class="container  ">
+    <div class="heading_container heading_center">
+      <h2>JAVASCRIPT</h2>
+      <p>Let's understand the fundamental and basic concept of HTML</p>
+      <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
+      <a href="coming_soon.php">LEARN JAVASCRIPT</a>
     </div>
-  </section>
-  <!--PHP Section End-->
+  </div>
+</section>
+<!--JS Section End-->
 
-  <!--MYSQL Section Start-->
-  <section class="about_section layout_padding about-sql">
-    <div class="container  ">
-      <div class="heading_container heading_center">
-        <h2>MYSQL</h2>
-        <p>Let's understand the fundamental and basic concept of HTML</p>
-        <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
-        <a href="coming_soon.php">LEARN MYSQL</a>
-      </div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6521349394702804"
+     crossorigin="anonymous"></script>
+<!-- Ad for header -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6521349394702804"
+     data-ad-slot="3555489595"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<!--PHP Section Start-->
+<section class="about_section layout_padding about-php">
+  <div class="container  ">
+    <div class="heading_container heading_center">
+      <h2>PHP</h2>
+      <p>Let's understand the fundamental and basic concept of HTML</p>
+      <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
+      <a href="coming_soon.php">LEARN PHP</a>
     </div>
-  </section>
-  <!--MYSQL Section End-->
-<?php require("footer.php"); ?>  
+  </div>
+</section>
+<!--PHP Section End-->
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6521349394702804"
+     crossorigin="anonymous"></script>
+<!-- Ad for header -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6521349394702804"
+     data-ad-slot="3555489595"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<!--MYSQL Section Start-->
+<section class="about_section layout_padding about-sql">
+  <div class="container  ">
+    <div class="heading_container heading_center">
+      <h2>MYSQL</h2>
+      <p>Let's understand the fundamental and basic concept of HTML</p>
+      <p>We will teach you the basic and essentials of HTML in this course Click the button below to learn.</p>
+      <a href="coming_soon.php">LEARN MYSQL</a>
+    </div>
+  </div>
+</section>
+<!--MYSQL Section End-->
+<?php require("footer.php"); ?>
