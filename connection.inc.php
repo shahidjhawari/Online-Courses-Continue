b27bac50425cea958092c87f164a5e528f603df2
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 21:52:17 +0500

    latest

--- connection.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$con=mysqli_connect("localhost","root","","nawab2");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/');
-define('SITE_PATH','http://127.0.0.1/');
-
-define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
-?>
---
+++ ecom/product.php
@@ -0,0 +1,55 @@
+<?php
+require('top.php');
+if (isset($_GET['id'])) {
+    $product_id = mysqli_real_escape_string($con, $_GET['id']);
+    if ($product_id > 0) {
+        $get_product = get_product($con, '', '', $product_id);
+    } else {
+?>
+        <script>
+            window.location.href = 'index.php';
+        </script>
+    <?php
+    }
+} else {
+    ?>
+    <script>
+        window.location.href = 'index.php';
+    </script>
+<?php
+}
+?>
+
+<!-- YouTube Video Section start -->
+<div class="container yt-video-html-box">
+    <div class="video-wrapper">
+        <iframe src="https://www.youtube.com/embed/uLXNGMQW-R0?si=w8ZGsOcfQyitlukK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+</div>
+<!-- YouTube Video Section start -->
+
+
+<!-- all course add heading start -->
+<div class="domain tag-heading">
+    <div class="titlepage text_align_center">
+        <h2><?php echo $get_product['0']['name']?> TAG <span class="blue_light">EXAMPLE</span></h2>
+    </div>
+</div>
+<!-- all course add heading start -->
+
+<!--Tag Section Start-->
+<div class="container">
+    <div id="carouselExample" class="carousel slide inner-css">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$get_product['0']['image']?>" class="d-block w-100" alt="a tag html">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container d-flex align-items-center justify-content-center download-btn">
+    <a href="img/a-tag.png" download="a tag - NAWAB ACADEMY" class="btn btn-danger">Download</a>
+</div>
+<!--Tag Section End-->
+
+<?php require('footer.php'); ?>
