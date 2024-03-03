132c3381698030c0de3413cf0204679dd9986161
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 21:18:57 +0500

    added categories pg

---
+++ categories.php
@@ -0,0 +1,21 @@
+<?php require('top.php'); ?>
+
+<!--HTML All Tags Section Start-->
+<div class="container">
+    <!-- Courses Heading Start -->
+    <div class="form html-tags-search">
+        <div class="searchbar">
+            <input class="search_input" type="text" id="searchInput" placeholder="Search tags...">
+        </div>
+    </div>
+    <!-- Courses Heading End -->
+    <div class="list-group all-html-tags">
+        <code>
+            <a href="html/a.html" class="list-group-item list-group-item-action tag-css"><span>
+                    << /span><span class="html-tag">a</span><span>></span> Defines a hyperlink</a>
+        </code>
+    </div>
+</div>
+<!--HTML All Tags Section End-->
+
+<?php require('footer.php'); ?>
