ed7547ce260b4293cb8f8d2a352d88e34d5d6a98
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 20:23:42 +0500

    latest

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
   <script src="js/script.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="js/main.js"></script>
+
   <!--Font Awesome files Here-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css">
 </body>
--- top.php
+++ top.php
@@ -28,11 +28,11 @@ while ($row = mysqli_fetch_assoc($cat_res)) {
   <meta name="author" content="MAHAR SHAHID IQBAL JHAWARI">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NAWAB ACADEMY</title>
-
+  
   <!--Bootstrap files Here-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+  
   <!--Own files Here-->
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/responsive.css">
@@ -43,31 +43,30 @@ while ($row = mysqli_fetch_assoc($cat_res)) {
 
   <!--Header Section Start-->
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-      <div class="container container-css">
-        <a href="index.php"><img src="img/logo_bg_remove.png" alt="NAWAB LOGO" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan" width="50px"></a>
-        <a class="navbar-brand" href="index.php" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan"><b>NAWAB
-            ACADEMY</b></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="coming_soon.htmlnavbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php"><b>HOME</b></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><b>COURSES</b></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><b>PROJECTS</b></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><b>SIGN UP</b></a>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+    <div class="container container-css">
+      <a href="index.php"><img src="img/logo_bg_remove.png" alt="NAWAB LOGO" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan" width="50px"></a>
+      <a class="navbar-brand" href="index.php" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan"><b>NAWAB ACADEMY</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php"><b>HOME</b></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><b>COURSES</b></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><b>PROJECTS</b></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><b>SIGN UP</b></a>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </header>
+    </div>
+  </nav>
+</header>
   <!--Header Section End-->
