e0ad7519c3a99c7654d8de7a66973df9acee18a6
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 20:13:04 +0500

    latest

--- top.php
+++ top.php
@@ -1,15 +1,16 @@
 <?php
 require('connection.inc.php');
 require('functions.inc.php');
-$cat_res=mysqli_query($con,"select * from categories where status=1 order by categories asc");
-$cat_arr=array();
-while($row=mysqli_fetch_assoc($cat_res)){
-	$cat_arr[]=$row;	
+$cat_res = mysqli_query($con, "select * from categories where status=1 order by categories asc");
+$cat_arr = array();
+while ($row = mysqli_fetch_assoc($cat_res)) {
+  $cat_arr[] = $row;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <!--All Meta Tags Here-->
   <meta charset="UTF-8">
@@ -23,34 +24,31 @@ while($row=mysqli_fetch_assoc($cat_res)){
     nawab academy app development, jhawarian no 1 academy, sargodha no 1 academy, pakistan no 1 academy, html, 
     css, java script, php, my sql, python,ethical hacking, html course, css course, java script course, php course, 
     my sql course, python course, ethical hacking course,">
-  <meta name="discription"
-    content="NAWAB Web Development acdemy in Sargodha, Jhawarian, HTLM, CSS, JS, PHP, MYSQL, PYTHON online & classes courses">
+  <meta name="discription" content="NAWAB Web Development acdemy in Sargodha, Jhawarian, HTLM, CSS, JS, PHP, MYSQL, PYTHON online & classes courses">
   <meta name="author" content="MAHAR SHAHID IQBAL JHAWARI">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NAWAB ACADEMY</title>
-  
+
   <!--Bootstrap files Here-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  
+
   <!--Own files Here-->
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/responsive.css">
   <link rel="icon" href="img/logo_bg_remove.png">
 </head>
+
 <body>
 
   <!--Header Section Start-->
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-warning">
       <div class="container container-css">
-        <a href="index.php"><img src="img/logo_bg_remove.png" alt="NAWAB LOGO"
-            title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan" width="50px"></a>
+        <a href="index.php"><img src="img/logo_bg_remove.png" alt="NAWAB LOGO" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan" width="50px"></a>
         <a class="navbar-brand" href="index.php" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan"><b>NAWAB
             ACADEMY</b></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-          data-bs-target="coming_soon.htmlnavbarNav" aria-controls="navbarNav" aria-expanded="false"
-          aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="coming_soon.htmlnavbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
