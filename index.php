1a86b638cc814957a9a5f58a44a7fb5b838d0376
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 19:57:29 +0500

    top, footer & index into php

--- index.php
+++ index.php
@@ -1,70 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require('top.php'); ?>
 
-<head>
-  <!--All Meta Tags Here-->
-  <meta charset="UTF-8">
-  <meta name="keyword" content="nawab, nawab academy, academy, shahid, shahid iqbal, shahid iqbal jhawari, mahar shahid iqbal jhawari, course, 
-    courses, nawab courses, nawab academy courses, nawab academy course, 
-    shahid iqbal course, nawab html course, nawab css course, 
-    nawab javascript course, nawab html, nawab css, nawab java script, 
-    jhawarian, jhawarian academy, jhawarian courses, jhawarian web development, 
-    web development, web development course, web development nawab, nawab web development, 
-    nawab ethical hacking, nawab app development, app development course, android app development course, 
-    nawab academy app development, jhawarian no 1 academy, sargodha no 1 academy, pakistan no 1 academy, html, 
-    css, java script, php, my sql, python,ethical hacking, html course, css course, java script course, php course, 
-    my sql course, python course, ethical hacking course,">
-  <meta name="discription"
-    content="NAWAB Web Development acdemy in Sargodha, Jhawarian, HTLM, CSS, JS, PHP, MYSQL, PYTHON online & classes courses">
-  <meta name="author" content="MAHAR SHAHID IQBAL JHAWARI">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>NAWAB ACADEMY</title>
-  <!--Own files Here-->
-  <link rel="stylesheet" href="css/styles.css">
-  <link rel="stylesheet" href="css/responsive.css">
-  <link rel="icon" href="img/logo_bg_remove.png">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6521349394702804"
-    crossorigin="anonymous"></script>
-</head>
-
-<body>
-
-  <!--Header Section Start-->
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-      <div class="container container-css">
-        <a href="index.html"><img src="img/logo_bg_remove.png" alt="NAWAB LOGO"
-            title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan" width="50px"></a>
-        <a class="navbar-brand" href="index.html" title="NAWAB ACADEMY - Jhawarian, Sargodha, Punjab, Pakistan"><b>NAWAB
-            ACADEMY</b></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-          data-bs-target="coming_soon.htmlnavbarNav" aria-controls="navbarNav" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html"><b>HOME</b></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="coming_soon.html"><b>COURSES</b></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="coming_soon.html"><b>PROJECTS</b></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="coming_soon.html"><b>SIGN UP</b></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div id="progress"></div>
-  </header>
-  <!--Header Section End-->
-
-  <!--Banner Section Start-->
+<!--Banner Section Start-->
   <div id="carouselExampleInterval" class="carousel slide inner-css" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="5000">
@@ -278,22 +214,4 @@
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 
-  <!--Footer Section Start-->
-  <footer class="bg-warning text-dark text-center py-3">
-    <div class="container">
-      &copy; <b>2023 NAWAB ACADEMY. All rights reserved.</b>
-    </div>
-  </footer>
-  <!--Footer Section End-->
-
-  <!--Java Script Files Here Start-->
-  <script defer async src="js/script.js"></script>
-  <script defer async src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <!--Font Awesome files Here-->
-  <link media="all" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <!--Bootstrap files Here-->
-  <link media="all" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script defer async src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+<?php require('footer.php'); ?>
