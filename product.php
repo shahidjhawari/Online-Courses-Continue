3f72ed31a2c72145976db8ee6119cdef697aa92c
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 21:30:33 +0500

    adding product pg

---
+++ product.php
@@ -0,0 +1,25 @@
+<?php 
+require('top.php');
+if(isset($_GET['id'])){
+	$product_id=mysqli_real_escape_string($con,$_GET['id']);
+	if($product_id>0){
+		$get_product=get_product($con,'','',$product_id);
+	}else{
+		?>
+		<script>
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+}else{
+	?>
+	<script>
+	window.location.href='index.php';
+	</script>
+	<?php
+}
+?>
+
+
+
+<?php require('footer.php'); ?>
