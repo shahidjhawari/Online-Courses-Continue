56829b744d984899bb9d6675aaed56bda9f5a7c3
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-03 22:20:03 +0500

    all done

--- ecom/top.php
+++ ecom/top.php
@@ -1,7 +1,7 @@
 <?php
 require('connection.inc.php');
 require('functions.inc.php');
-$cat_res = mysqli_query($con, "select * from categories where status=1 order by categories asc");
+$cat_res = mysqli_query($con, "select * from categories where status=1 order by categories DESC");
 $cat_arr = array();
 while ($row = mysqli_fetch_assoc($cat_res)) {
   $cat_arr[] = $row;
