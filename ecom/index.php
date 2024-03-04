79694248dc0a5daf392b772806a9a853acc8be70
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-04 22:53:13 +0500

    banner set

--- ecom/index.php
+++ ecom/index.php
@@ -1,23 +1,23 @@
 <?php require('top.php'); ?>
 
 <!--Banner Section Start-->
-<div id="carouselExampleInterval" class="carousel slide inner-css" data-bs-ride="carousel">
+<div id="carouselExample" class="carousel slide inner-css">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="5000">
-      <img src="img/banner1.webp" class="d-block w-100" alt="..." width="100%" height="100%">
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <a href="openai.php"><img src="img/ai-banner2.webp" class="d-block w-100" alt="..." width="100%" height="100%"></a>
+    <div class="carousel-item active">
+      <img src="img/banner1.webp" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="img/banner1.webp" class="d-block w-100" alt="..." width="100%" height="100%">
+      <img src="img/ai-banner2.webp" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner1.webp" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="coming_soon.htmlcarouselExampleInterval" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="coming_soon.htmlcarouselExampleInterval" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
