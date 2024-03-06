1ef9dac3035b768b4f063d7185873f4dfae6beb5
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 21:21:46 +0500

    manipulate admin latest

--- ecom/admin/footer.inc.php
+++ ecom/admin/footer.inc.php
@@ -10,9 +10,7 @@
             </div>
          </footer>
       </div>
-      <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
-      <script src="assets/js/popper.min.js" type="text/javascript"></script>
-      <script src="assets/js/plugins.js" type="text/javascript"></script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
       <script src="assets/js/main.js" type="text/javascript"></script>
    </body>
 </html>
--- ecom/admin/top.inc.php
+++ ecom/admin/top.inc.php
@@ -17,15 +17,8 @@ date_default_timezone_set('Asia/Karachi');
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title>Dashboard Page</title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="assets/css/normalize.css">
       <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-      <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-      <link rel="stylesheet" href="assets/css/themify-icons.css">
-      <link rel="stylesheet" href="assets/css/pe-icon-7-filled.css">
-      <link rel="stylesheet" href="assets/css/flag-icon.min.css">
-      <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
       <link rel="stylesheet" href="assets/css/style.css">
-      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
    </head>
    <body>
       <aside id="left-panel" class="left-panel">
