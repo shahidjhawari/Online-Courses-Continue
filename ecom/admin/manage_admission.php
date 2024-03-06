ccff67eb1a5894fecc0144256242865ad9a0857e
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 21:58:47 +0500

    latest

--- ecom/admin/manage_admission.php
+++ ecom/admin/manage_admission.php
@@ -1,21 +1,7 @@
 <?php require('top.inc.php');
-// Database connection parameters
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "nawab8";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Query database to fetch admissions data
 $sql = "SELECT * FROM admissions";
-$result = $conn->query($sql);
+$result = $con->query($sql);
 ?>
 
 <div class="container">
