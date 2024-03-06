4f5f4f2dd2886ac1301c4e4852108ed973528d17
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 17:56:10 +0500

    latest

--- ecom/footer.php
+++ ecom/footer.php
@@ -5,7 +5,7 @@
     </div>
   </footer>
   <!--Footer Section End-->
-
+  
   <!--Java Script Files Here Start-->
   <script src="js/script.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
