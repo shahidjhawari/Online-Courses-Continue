ce27f923667fd8bbf5c38e3dfe57e6a81f02b01e
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 21:15:15 +0500

    latest

--- ecom/admin/connection.inc.php
+++ ecom/admin/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $con=mysqli_connect("localhost","root","","nawab8");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/nawab/ecom/');
-define('SITE_PATH','http://127.0.0.1/nawab/ecom/');
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/');
+define('SITE_PATH','http://127.0.0.1/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
