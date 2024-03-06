7d916e7e079b39035b802f487e6cdabf921d7b4b
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 20:55:02 +0500

    online connection latest

--- ecom/admin/connection.inc.php
+++ ecom/admin/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $con=mysqli_connect("sql204.infinityfree.com","if0_35562447","o3kuMy0BlkrjV","if0_35562447_nawab");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/nawab/ecom/');
-define('SITE_PATH','https://nawab.website/nawab/ecom/');
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/');
+define('SITE_PATH','https://nawab.website/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
