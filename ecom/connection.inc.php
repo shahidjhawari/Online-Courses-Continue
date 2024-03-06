c807ede13759ec705b1c6c3eb94d0b9fc73d666c
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 20:42:24 +0500

    online connection

--- ecom/connection.inc.php
+++ ecom/connection.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","nawab8");
+$con=mysqli_connect("sql204.infinityfree.com","if0_35562447","o3kuMy0BlkrjV","if0_35562447_nawab");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/nawab/ecom/');
 define('SITE_PATH','http://127.0.0.1/nawab/ecom/');
 
