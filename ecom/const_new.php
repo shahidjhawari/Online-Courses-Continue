b7135206c3f6c7891dba325d9e9e4d723ec01088
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-06 08:47:25 +0500

    latest

--- ecom/const_new.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/nawab/ecom/');
-define('SITE_PATH','http://127.0.0.1/nawab/ecom/');
-
-define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
-?>
