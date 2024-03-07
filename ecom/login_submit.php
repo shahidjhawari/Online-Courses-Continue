27937cd04d5deea1af0830c66a03c7f958cae081
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-07 09:05:50 +0500

    user login for 1 year

--- ecom/login_submit.php
+++ ecom/login_submit.php
@@ -2,24 +2,30 @@
 require('connection.inc.php');
 require('functions.inc.php');
 
-$email=get_safe_value($con,$_POST['email']);
-$password=get_safe_value($con,$_POST['password']);
+$email = get_safe_value($con, $_POST['email']);
+$password = get_safe_value($con, $_POST['password']);
 
-$res=mysqli_query($con,"select * from users where email='$email' and password='$password'");
-$check_user=mysqli_num_rows($res);
-if($check_user>0){
-	$row=mysqli_fetch_assoc($res);
-	$_SESSION['USER_LOGIN']='yes';
-	$_SESSION['USER_ID']=$row['id'];
-	$_SESSION['USER_NAME']=$row['name'];
-	
-	if(isset($_SESSION['WISHLIST_ID']) && $_SESSION['WISHLIST_ID']!=''){
-		wishlist_add($con,$_SESSION['USER_ID'],$_SESSION['WISHLIST_ID']);
-		unset($_SESSION['WISHLIST_ID']);
-	}
-	
-	echo "valid";
-}else{
-	echo "wrong";
+$res = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND password='$password'");
+$check_user = mysqli_num_rows($res);
+if ($check_user > 0) {
+    $row = mysqli_fetch_assoc($res);
+    $_SESSION['USER_LOGIN'] = 'yes';
+    $_SESSION['USER_ID'] = $row['id'];
+    $_SESSION['USER_NAME'] = $row['name'];
+
+    // Extend session lifetime to 1 year (31536000 seconds)
+    ini_set('session.cookie_lifetime', 31536000);
+
+    // Set session cookie parameters to extend session to 1 year
+    session_set_cookie_params(31536000);
+
+    if (isset($_SESSION['WISHLIST_ID']) && $_SESSION['WISHLIST_ID'] != '') {
+        wishlist_add($con, $_SESSION['USER_ID'], $_SESSION['WISHLIST_ID']);
+        unset($_SESSION['WISHLIST_ID']);
+    }
+
+    echo "valid";
+} else {
+    echo "wrong";
 }
 ?>
