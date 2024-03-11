4684572ca024e4b20a8c8105b29722f34346bdcc
Lodrmssi007&github <shahidiqbaljhawari@gmail.com>
2024-03-11 09:50:08 +0500

    latest

--- students.php
+++ students.php
@@ -42,9 +42,13 @@ $result = $con->query($sql);
                         <p>Father Name : <?php echo $row["father_name"] ?></p>
                         <p style="margin-top: -20px;">Course : <?php echo $row["select_option"] ?></p>
                         <p style="margin-top: -20px;">Date : <?php echo $row["created_at"] ?></p>
-                        <p style="margin-top: -20px;">Status : <?php echo $row["status"] ?></p>
+                        <p style="margin-top: -20px;">Status : <?php echo $row["status"] . "<br>" . $row["fee"] ?></p>
                         <?php if (!empty($row['certificate'])) { ?>
                             <a href="<?php echo CERTIFICATE_FILE_SITE_PATH . $row['certificate']; ?>" download class="btn btn-warning btn-sm">Download Certificate</a>
+                        <?php } ?>
+                        <?php if (!empty($row['fee'])) { ?>
+                            <button onclick="fee()" class="btn btn-danger btn-sm">Last Fee Pending</button><br>
+                            <small>Pay fee to get certificate</small>
                         <?php } ?>
                     </div>
                 </div>
@@ -55,4 +59,10 @@ $result = $con->query($sql);
         ?>
     </div>
 </div>
+
+<script>
+    function fee() {
+        alert("Please pay fee to get certificate!")
+    }
+</script>
 <?php require('footer.php'); ?>
